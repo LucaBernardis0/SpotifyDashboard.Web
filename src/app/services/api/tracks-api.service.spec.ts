230e8da8c8712e5980e4bde13fc115647ba4628a
LucaBernardis0 <luca.bernardis@beantech.it>
2024-06-25 09:25:44 +0200

    created services files for api calls to the backend

---
+++ src/app/services/api/tracks-api.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TracksApiService } from './tracks-api.service';
+
+describe('TracksApiService', () => {
+  let service: TracksApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TracksApiService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
