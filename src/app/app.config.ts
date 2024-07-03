35e6037d1b6b0fef1fa938304d979e5bf651b63a
LucaBernardis0 <luca.bernardis@beantech.it>
2024-07-03 16:29:21 +0200

    Removing Hydration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { httpInterceptor } from './http.interceptot';
 import { SpotifyAuthService } from './services/spotify-auth.service';
@@ -15,7 +14,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(withInterceptors([httpInterceptor]), withFetch()),
     {
       // When the application is initialized it calls the method the check the authentication and eventually redirect to the spotify authentication page
